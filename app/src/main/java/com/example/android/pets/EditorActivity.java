71029c89ac0d4cda00baca3bad02a72295dda2a3
Ray Linares <rayl77@gmail.com>
2018-02-12 14:11:14 -0600

    Removed "Delete" menu item from Editor Activity when entering a new pet

--- app/src/main/java/com/example/android/pets/EditorActivity.java
+++ app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -98,6 +98,9 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         //we can set the title of the activity for a New Pet
         if (mCurrentPetUri == null){
             setTitle(getString(R.string.editor_activity_title_new_pet));
+            // Invalidate the options menu, so the "Delete" menu option can be hidden.
+            // (It doesn't make sense to delete a pet that hasn't been created yet.)
+            invalidateOptionsMenu();
         } else {
             //otherwise we know it is an existing entry so we can set title of activity to Edit Pet
             setTitle(getString(R.string.editor_activity_title_edit_pet));
@@ -241,6 +244,21 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         // Inflate the menu options from the res/menu/menu_editor.xml file.
         // This adds menu items to the app bar.
         getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    /**
+     * This method is called after invalidateOptionsMenu(), so that the
+     * menu can be updated (some menu items can be hidden or made visible).
+     */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        // If this is a new pet, hide the "Delete" menu item.
+        if (mCurrentPetUri == null){
+            MenuItem menuItem = menu.findItem(R.id.action_delete);
+            menuItem.setVisible(false);
+        }
         return true;
     }
 
